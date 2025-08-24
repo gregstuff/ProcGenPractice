ec442df3c9f1b0aacd10d9320901164fe81f08e5
Greg Acton <gregdevappl@gmail.com>
2025-08-24 09:06:36 +0800

    minor refactor and init debug texture decoration class

---
+++ Assets/Scripts/Map/Decoration/Impl/DebugTextureDecoratorSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DebugTextureDecoratorSO : DecoratorSO
+{
+    public override void ApplyDecorations(ICapabilityProvider level)
+    {
+        throw new System.NotImplementedException();
+    }
+}
